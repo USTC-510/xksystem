14de22d6ee69655eb3b7270fde437787bbfdf3f5
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-05-16 17:15:44 +0800

    最近的修改

--- src/main/java/com/pojo/Administrator.java
+++ src/main/java/com/pojo/Administrator.java
@@ -32,7 +32,6 @@ public class Administrator
     public int getAge() {return age;}
 
     public void setAge(int age) {this.age = age;}
-
     @Override
     public String toString() {return this.name;}
 }
