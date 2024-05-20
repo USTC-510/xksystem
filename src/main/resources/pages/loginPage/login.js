876c9898adb5001a9c23cc8b36056a0e2ceb1a60
afron8 <haha123@mail.ustc.edu.cn>
2024-05-20 10:19:44 +0800

    加入了登陆身份的按钮，稍微改变了登陆匹配的逻辑

--- src/main/resources/pages/loginPage/login.js
+++ src/main/resources/pages/loginPage/login.js
@@ -16,19 +16,19 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var username = document.getElementById('username').value;  
     var password = document.getElementById('password').value;  
   
-    if () {  
-        alert('登录成功！');
-        window.location.href = 'studentInformation.html';  
+    if (username == 'a' && password == '1' && document.getElementById('student').checked) {  
+        alert('学生登录成功！');
+        //window.location.href = 'studentInformation.html';  
     } 
-    else if (){  
-        alert('登录成功！');
-        window.location.href = 'teacherInformation.html';
+    else if (username == 'b' && password == '2' && document.getElementById('teacher').checked){  
+        alert('老师登录成功！');
+        //window.location.href = 'teacherInformation.html';
     }  
-    else if (){
-        alert('登录成功！');
-        window.location.href = 'administratorInformation.html';
+    else if (username == 'c' && password == '3' && document.getElementById('admin').checked){
+        alert('管理员登录成功！');
+        //window.location.href = 'administratorInformation.html';
     }
     else{
-        document.getElementById('errorMessage').textContent = '用户名或密码错误！';
+        document.getElementById('errorMessage').textContent = '用户名或密码错误，或者您所选的登陆身份有误！';
     }
 });
