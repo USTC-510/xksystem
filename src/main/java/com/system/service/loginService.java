695a8c0e32e57ef2a9f6523464288673663a1bdc
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-06-02 23:31:41 +0800

    最近

--- src/main/java/com/system/service/loginService.java
+++ src/main/java/com/system/service/loginService.java
@@ -1,4 +1,82 @@
 package com.system.service;
 
-public class loginService {
+import com.system.mapper.AdministratorMapper;
+import com.system.mapper.StudentMapper;
+import com.system.mapper.TeacherMapper;
+import com.system.pojo.Administrator;
+import com.system.pojo.Student;
+import com.system.pojo.Teacher;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoginService {
+
+    @Resource
+    private AdministratorMapper administratorMapper;
+
+    @Resource
+    private StudentMapper studentMapper;
+
+    @Resource
+    private TeacherMapper teacherMapper;
+
+    public int login(String username, String password, int identity) {
+        if(identity==1){
+            //学生登录
+            List<Student> list=studentMapper.selectbyId(username);
+            if(list!=null&&!list.isEmpty()){
+                Student student=list.get(0);
+                if(password.equals(student.getPassword())) {
+                    return 1;
+                }
+                else{
+                    return 0;
+                }
+            }
+            else{
+                return 0;
+            }
+
+        }
+        else if(identity==2){
+            //教师登录
+            List<Teacher> list=teacherMapper.selectById(username);
+            if(list!=null&&!list.isEmpty()){
+                Teacher teacher=list.get(0);
+                if(password.equals(teacher.getPassword())) {
+                    return 2;
+                }
+                else{
+                    return 0;
+                }
+
+            }
+            else{
+                return 0;
+            }
+        }
+        else if(identity==3){
+            //管理员登录
+            List<Administrator> list= administratorMapper.selectById(username);
+            if(list!=null&&!list.isEmpty()){
+                Administrator administrator=list.get(0);
+                if(password.equals(administrator.getPassword())) {
+                    return 3;
+                }
+                else{
+                    return 0;
+                }
+
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            return 0;
+        }
+    }
 }
