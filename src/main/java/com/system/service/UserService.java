e620b53cb4a04c6fa597da5ce0def93f6043ef51
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-06-06 20:32:31 +0800

    最近的更新

--- src/main/java/com/system/service/UserService.java
+++ src/main/java/com/system/service/UserService.java
@@ -1,11 +1,17 @@
 package com.system.service;
 
+import com.system.pojo.User;
+
 public interface UserService
 {
-   int login(String code,String password,String identity);
+   User login(String code, String password, String identity);
     /*用户的登录验证。
       前端需要在参数中包含账户名username（我们使用学号和工号作为账户名,在后端中称为code），密码password和身份identity（student，teacher或administrator）
       返回值：0 -> 错误,并返回错误信息，前端需要在页面上展示错误信息; 1 -> 学生， 2 -> 老师, 3 -> 管理员
       接口的URL是：/api/user/login
       */
+   //int changePassword(String username,String password,String new_password);
+   /*修改密码
+     前端需要在参数中包含账户名username，原密码password和新密码new_password
+   */
 }
--- src/main/java/com/system/service/impl/UserServiceImpl.java
+++ src/main/java/com/system/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.system.service.impl;
+
 import com.system.mapper.AdministratorMapper;
 import com.system.mapper.StudentMapper;
 import com.system.mapper.TeacherMapper;
 import com.system.pojo.Administrator;
 import com.system.pojo.Student;
 import com.system.pojo.Teacher;
+import com.system.pojo.User;
 import com.system.service.UserService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
@@ -18,27 +20,28 @@ public class UserServiceImpl implements UserService
    private TeacherMapper teacherMapper;
    @Resource
    private AdministratorMapper administratorMapper;
-   public int login(String code,String password,String identity)
+
+   public User login(String code, String password, String identity)
    {
        switch(identity)
        {
            case "1":
                Student student = studentMapper.selectByCode(code);
-               if (student == null) {return 0;}
-               else if (student.getPassword().equals(password)) {return 1;}
-               else {return 0;}
+               if (student == null) {return null;}
+               else if (student.getPassword().equals(password)) {return student;}
+               else {return null;}
            case "2":
                Teacher teacher = teacherMapper.selectByCode(code);
-               if (teacher == null) {return 0;}
-               else if (teacher.getPassword().equals(password)) {return 2;}
-               else {return 0;}
+               if (teacher == null) {return null;}
+               else if (teacher.getPassword().equals(password)) {return teacher;}
+               else {return null;}
            case "3":
                Administrator admin = administratorMapper.selectByCode(code);
-               if (admin == null) {return 0;}
-               else if (admin.getPassword().equals(password)) {return 3;}
-               else {return 0;}
+               if (admin == null) {return null;}
+               else if (admin.getPassword().equals(password)) {return admin;}
+               else {return null;}
            default:
-               return 0;
+               return null;
        }
    }
 }
