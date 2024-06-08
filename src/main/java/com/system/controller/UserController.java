60b048b17a04df7dc55be5fb4186f6858068454d
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-06-08 21:36:42 +0800

    新的

--- src/main/java/com/system/controller/UserController.java
+++ src/main/java/com/system/controller/UserController.java
@@ -3,9 +3,7 @@ package com.system.controller;
 import com.system.pojo.Result;
 import com.system.pojo.User;
 import com.system.service.UserService;
-import com.system.tool.CookieUser;
 import jakarta.annotation.Resource;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -16,18 +14,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = {"http://localhost:8080","http://114.214.234.245:8080"}, methods = {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin(origins = {"http://115.236.153.170","http://localhost:8080","http://114.214.234.245:8080","http://92o66n2830.goho.co"}, methods = {RequestMethod.GET, RequestMethod.POST})
 public class UserController
 {
     @Resource
     private UserService userService;
     //获得service层接口的代理
 
-
     @PostMapping("/login")
     public Result login(@RequestBody UserLoginDTO userLoginDTO, HttpServletRequest request, HttpServletResponse response)
-    //实现登录验证
     {
+        //实现登录验证
+
         String code = userLoginDTO.getUsername();
         String password = userLoginDTO.getPassword();
         String identity = userLoginDTO.getIdentity();
@@ -48,41 +46,17 @@ public class UserController
             //设置请求头
 
             if (this_User == null){return Result.error("账号或密码错误！","notFound");}
-            else
-            {
-                Cookie usernameCookie = new Cookie("username",this_User.getCode());
-                usernameCookie.setMaxAge(60*60*24*7);
-                usernameCookie.setPath("/");
-                response.addCookie(usernameCookie);
-
-                Cookie nameCookie = new Cookie("name",this_User.getName());
-                nameCookie.setMaxAge(60*60*24*7);
-                nameCookie.setPath("/");
-                response.addCookie(nameCookie);
-
-                Cookie identityCookie = new Cookie("identity",this_User.getIdentity());
-                identityCookie.setMaxAge(60*60*24*7);
-                identityCookie.setPath("/");
-                response.addCookie(identityCookie);
-                //设置Cookies
-
-                return Result.success(this_User.getIdentity());
-            }
+            else {return Result.success(this_User.getIdentity());}
         }
     }
 
-    @GetMapping("/realName")
-    public Result realName(HttpServletRequest request, HttpServletResponse response)
-    {
-        //获取当前用户名
-
-        Cookie[] cookies = request.getCookies();
-        String name = CookieUser.getValue(cookies,"name");
-        //调用工具类CookieUser获取cookies中的用户名
-
-        if (name != null) {return Result.success(name);}
-        else {return Result.error("用户未登录！","notfound");}
-    }
+//    @GetMapping("/realName")
+//    public Result realName(@RequestParam String username,HttpServletRequest request, HttpServletResponse response)
+//    {
+//        //获取当前用户的姓名
+//
+//
+//    }
 }
 
 @Data
--- src/main/resources/pages/vue-project/vue.config.js
+++ src/main/resources/pages/vue-project/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: '92o66n2830.goho.co'
+  }
 })
