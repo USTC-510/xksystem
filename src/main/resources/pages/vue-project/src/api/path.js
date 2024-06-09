6364fd88205a2e6f1c8b108041ad729b2f20e7dc
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-06-09 14:56:51 +0800

    修改错误

--- src/main/resources/pages/vue-project/src/api/path.js
+++ src/main/resources/pages/vue-project/src/api/path.js
@@ -4,9 +4,9 @@ const base = {
     login:"api/user/login",
     realName:"api/user/realName",
     infor:"api/user/getInfor",
-    changePassword:"api/user/changePassword"
-    allCourses:"api/user/allCourse"
-    detailedCourse:"api/user/detailedCourse"
+    changePassword:"api/user/changePassword",
+    allCourses:"api/user/allCourse",
+    detailedCourse:"api/user/detailedCourse",
     //注意：baseurl和login拼起来才是真正的url路径，这么写是因为在一个程序中url前面那一串基本都是相同的，需要变动的常常是后面那一串
 }
 export default base;
