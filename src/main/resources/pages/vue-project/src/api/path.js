d45a21959b3a98dff195d14ebf0861ba36156b1f
afron8 <haha123@mail.ustc.edu.cn>
2024-06-09 12:43:27 +0800

    哈

--- src/main/resources/pages/vue-project/src/api/path.js
+++ src/main/resources/pages/vue-project/src/api/path.js
@@ -1,11 +1,12 @@
 
 const base = {
-    //baseurl:"http://localhost:1969/",
     baseurl:"http://114.214.234.245:1969/",
     login:"api/user/login",
     realName:"api/user/realName",
     infor:"api/user/infor",
-    ChangePassword:"api/user/changePassword"
+    changePassword:"api/user/changePassword"
+    allCourses:"api/user/allCourse"
+    detailedCourse:"api/user/detailedCourse"
     //注意：baseurl和login拼起来才是真正的url路径，这么写是因为在一个程序中url前面那一串基本都是相同的，需要变动的常常是后面那一串
 }
 export default base;
