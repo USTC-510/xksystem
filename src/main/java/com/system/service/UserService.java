7487cb3344ec143b06ba2797989c9f04701a09f3
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-06-09 10:10:43 +0800

    忘了写接口了

--- src/main/java/com/system/service/UserService.java
+++ src/main/java/com/system/service/UserService.java
@@ -5,4 +5,5 @@ import com.system.pojo.User;
 public interface UserService
 {
    User getUser(String username,String identity);
+   int changePassword(String username,String identity,String originalPassword,String newPassword);
 }
