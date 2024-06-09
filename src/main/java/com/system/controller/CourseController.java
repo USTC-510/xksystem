a38f8db59f6050c30ed67f839e480dcf6c53bf47
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-06-09 16:17:28 +0800

    S

--- src/main/java/com/system/controller/CourseController.java
+++ src/main/java/com/system/controller/CourseController.java
@@ -1,11 +1,15 @@
 package com.system.controller;
 
+import com.system.pojo.Course;
 import com.system.pojo.Result;
 import com.system.service.CourseService;
 import jakarta.annotation.Resource;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/course")
@@ -16,13 +20,19 @@ public class CourseController
     @GetMapping("/allCourse")
     public Result getAllCourses()
     {
+        List<Course> courses = courseService.getAllCourses();
+        if (ObjectUtils.isEmpty(courses)) {return Result.error("发生了意料之外的错误",null);}
+        else
+        {
+                
+        }
 
     }
 
     @GetMapping("/detailedCourse")
     public Result getDetailedCourse()
     {
-            
+
     }
 
 }
