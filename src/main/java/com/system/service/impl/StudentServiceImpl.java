0984fb6d27fb529cdc36b096d2090b1a598ebbf8
evanCai1164 <1164759627@qq.com>
2024-06-10 20:50:48 +0800

    1

--- src/main/java/com/system/service/impl/StudentServiceImpl.java
+++ src/main/java/com/system/service/impl/StudentServiceImpl.java
@@ -1,20 +1,29 @@
 package com.system.service.impl;
 
 import com.system.mapper.CourseMapper;
+import com.system.mapper.TimeSlotMapper;
 import com.system.pojo.Course;
+import com.system.pojo.TimeSlot;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class StudentServiceImpl
+public class  StudentServiceImpl
 {
      @Resource
      CourseMapper courseMapper;
+     @Resource
+    TimeSlotMapper timeSlotMapper;
 
      public List<Course> getCoursesByStudentId(String code) {
-        return courseMapper.getCoursesByStudentId(code);
+
+         List<Course> courses= courseMapper.getCoursesByStudentId(code);
+         for(Course course:courses){
+             course.setTimeSlots(timeSlotMapper.getTimeByCourseCode(course.getCode()));
+         }
+         return courses;
     }
 
 }
