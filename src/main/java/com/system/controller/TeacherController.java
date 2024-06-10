b7845382802c3fd9eac310cdc998749a8c10e48e
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-06-10 20:03:03 +0800

    S

--- src/main/java/com/system/controller/TeacherController.java
+++ src/main/java/com/system/controller/TeacherController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = {"http://ustc510.v7.idcfengye.com","http://localhost:8081","http://114.214.234.245:8081","http://localhost:8080","http://114.214.234.245:8080","http://92o66n2830.goho.co:80","http://115.236.153.170:80"}, methods = {RequestMethod.GET, RequestMethod.POST})
 public class TeacherController
 {
-    @Resource
-    TeacherService teacherService;
+//    @Resource
+//    TeacherService teacherService;
 
 //    @GetMapping("/getCourses")
 //    public Result getCourses(@)
