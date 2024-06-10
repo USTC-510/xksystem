5533afe803e1dd6f90fd5c1ab3908a4ae5370ef0
evanCai1164 <1164759627@qq.com>
2024-06-10 19:54:30 +0800

    1

--- src/main/java/com/system/service/TeacherService.java
+++ src/main/java/com/system/service/TeacherService.java
@@ -1,6 +1,10 @@
 package com.system.service;
 
+import com.system.pojo.Course;
+
+import java.util.List;
+
 public interface TeacherService
 {
-
+    List<Course> getTeacherCourses();
 }
--- src/main/java/com/system/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/system/service/impl/TeacherServiceImpl.java
@@ -1,10 +1,19 @@
 package com.system.service.impl;
 
+import com.system.mapper.CourseMapper;
+import com.system.mapper.TeacherMapper;
+import com.system.pojo.Course;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TeacherServiceImpl
 {
-    
+    @Resource
+    CourseMapper courseMapper;
+    public List<Course> getCoursesByTeacherId(String code){
+        return courseMapper.getCoursesByTeacherId(code);
+    }
 
 }
